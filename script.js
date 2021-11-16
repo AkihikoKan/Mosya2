ec727440f36edae57e3c1850abf2392587b37cac
Kan Akihiko <oasis.9100@gmail.com>
2021-11-17 00:55:00 +0900

    @media(max-width:600px) に ハンバーガーメニューの追加
    スマホ対応のメニューに変更

---
+++ script.js
@@ -0,0 +1,7 @@
+//ハンバーガーメニューの動作　✕に変化
+document.getElementById('menuButton').addEventListener('click', function () {
+  this.classList.toggle('open');
+  document.getElementById('nav').classList.toggle('open');
+  document.getElementById('header').classList.toggle('open');
+  document.getElementById('mask').classList.toggle('active2');
+})
